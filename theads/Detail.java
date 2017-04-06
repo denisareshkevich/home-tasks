87dbfac6b2ccab665abb701315083fcd490feb02
denisareshkevich <orehden@mail.ru>
2017-04-06 11:47:16 +0300

    Add files via upload

---
+++ theads/Detail.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Denis Areshkevich on 02.04.2017.
+ */
+public class Detail {
+
+    private String detailType;
+
+    public Detail(String detailType) {
+        this.detailType = detailType;
+    }
+
+    public String getDetailType() {
+        return detailType;
+    }
+
+    public void setDetailType(String detailType) {
+        this.detailType = detailType;
+    }
+}
